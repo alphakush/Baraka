7c5566cfe6f37b5462de767a56708b144895d867
Nicolas MANELLI <nicolas.manelli@epitech.eu>
2019-10-09 12:15:37 +0200

    Première version du site internet

--- constant/Colors.js
+++
@@ -1,9 +0,0 @@
-export default {
-  primary: '#f7287b',
-  accent: '#c717fc',
-  accentColor: '#ff6f00',
-  White: '#FFFFFF',
-  LightBlue: '#F5FCFF',
-  Gainsboro: '#DCDCDC',
-  Grey: '#808080'
-};
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {} from "react-google-maps"
+
+
+// function App() {
+//     return ("")
+// }
+
+export default function App() {
+  return <div>map here</div>
+}
