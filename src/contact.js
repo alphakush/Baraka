943423736b3a10a15b2645ca6f81c64ae3f1c2ab
Loris Sanchez <loris1.sanchez@epitech.eu>
2019-12-10 11:35:36 +0100

    Finition du formulaire de contact

--- src/contact.js
+++ src/contact.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from './App.css'
 
-class Contact extends Component {   
-    // async componentDidMount() {
-    //     var userData = await Api.post('/contact');
-    //     }
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            object: '',
+            message: ''
+        };
+    }
+
+    mySubmitHandler = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    //
+    //  this.state contient les données du formulaire
+    //
+    //  Vous pourrez accéder aux variables suivantes :
+    //
+    //  this.state.email        - email de l'utilisateur
+    //
+    //  this.state.object       - object du formulaire de contact
+    //
+    //  this.state.message      - corps de message du formulaire de contact
+    //
+
+    myChangeHandler = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({ [nam]: val });
+    }
     render() {
         return (
             <div style={{ width: '100%', height: '100%' }}>
@@ -13,23 +41,19 @@ class Contact extends Component {
                     <h1 className="text-center text-primary">Contact</h1>
                 </div>
                 <div className={styles.form}></div>
-                <form>
+                <form onSubmit={this.mySubmitHandler}>
                     <div className="form-group">
-                      <label htmlFor="email">Adresse mail</label>
-                      <input type="email" className="form-control" id="email" aria-describedby="emailHelp"/>
-                      <small id="emailHelp" className="form-text text-muted">Adresse mail qui sera utilisée pour notre newsletter.</small>
+                        <label htmlFor="email">Adresse mail</label>
+                        <input type='text' className="form-control" name='email' onChange={this.myChangeHandler} />
+                        <small name="emailHelp" className="form-text text-muted">Adresse mail qui sera utilisée pour notre newsletter.</small>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="subject">Object</label>
-                        <input type="text" className="form-control" id="subject"/>
+                        <label htmlFor="object">Object</label>
+                        <input type='text' className="form-control" name='object' onChange={this.myChangeHandler} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="message">Message</label>
-                        <textarea className="form-control" rows="8" id="message"></textarea>
-                    </div>
-                    <div className="form-group form-check">
-                        <input type="checkbox" className="form-check-input" id="check"/>
-                        <label className="form-check-label" htmlFor="check">Souscrire à la newsletter</label>
+                        <textarea type='text' className="form-control" rows="8" name='message' onChange={this.myChangeHandler}></textarea>
                     </div>
                     <button type="submit" className="btn btn-primary btn-block">Envoyer</button>
                 </form>
