f71c1b17c7558335c005392426e107627ea4f591
AlphaKush <anthonycalves@gmail.com>
2019-12-12 13:05:05 +0100

    update store

--- store/actions/AuthAction.js
+++ store/actions/AuthAction.js
@@ -6,7 +6,32 @@ export const TOGGLE_ERROR = "TOGGLE_ERROR";
 
 import Api from '../../api/api';
 import { AsyncStorage } from 'react-native';
+import * as Location from 'expo-location';
 
+// export const signIn = (email, password) => {
+//     return async dispatch => {
+//         try {
+//             const response = await Api.post('/signin', { email, password });
+//             let location = await Location.getCurrentPositionAsync({});
+//             await AsyncStorage.multiSet([
+//               ['token', response.data.token],
+//               ['email', response.data.user.email],
+//               ['username', response.data.user.username],
+//               ['userlatitude', location.coords.latitude.toString()],
+//               ['userlongitude', location.coords.longitude.toString()]
+//             ]);
+//             dispatch({ type: TOGGLE_SIGNIN,
+//               payload: response.data.token,
+//               payloademail: response.data.user.email,
+//               payloadusername : response.data.user.username,
+//               payloaduserlatitude : location.coords.latitude,
+//               payloaduserlongitude : location.coords.longitude
+//             });
+//         } catch (error) {
+//             dispatch({ type: TOGGLE_ERROR, payload: "Merci de bien vérifier votre E-mail ou votre mot de passe" });
+//         }
+//     };
+// };
 export const signIn = (email, password) => {
     return async dispatch => {
         try {
@@ -14,7 +39,19 @@ export const signIn = (email, password) => {
             await AsyncStorage.setItem('token', response.data.token);
             await AsyncStorage.setItem('email', response.data.user.email);
             await AsyncStorage.setItem('username', response.data.user.username);
-            dispatch({ type: TOGGLE_SIGNIN, payload: response.data.token, payloademail: response.data.user.email, payloadusername : response.data.user.username});
+            let location = await Location.getCurrentPositionAsync({});
+            const latitude = location.coords.latitude.toString();
+            const longitude = location.coords.longitude.toString();
+            await AsyncStorage.setItem('userlatitude', latitude);
+            await AsyncStorage.setItem('userlongitude', longitude);
+
+            dispatch({ type: TOGGLE_SIGNIN,
+              payload: response.data.token,
+              payloademail: response.data.user.email,
+              payloadusername : response.data.user.username,
+              payloaduserlatitude : latitude,
+              payloaduserlongitude : longitude
+            });
         } catch (error) {
             dispatch({ type: TOGGLE_ERROR, payload: "Merci de bien vérifier votre E-mail ou votre mot de passe" });
         }
--- store/reducers/AuthReducer.js
+++ store/reducers/AuthReducer.js
@@ -10,19 +10,33 @@ const initialState = {
     token: null,
     errorMessage: '',
     username : '',
-    email : ''
+    email : '',
+    userlatitude : null,
+    userlongitude : null
 }
 
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case TOGGLE_SIGNIN:
-            return { ...state, token: action.payload, email: action.payloademail, username: action.payloadusername }
+            return { ...state,
+              token: action.payload,
+              email: action.payloademail,
+              username: action.payloadusername,
+              userlatitude : action.payloaduserlatitude,
+              userlongitude : action.payloaduserlongitude
+            }
         case TOGGLE_ERROR:
             return { ...state, errorMessage: action.payload }
         case TOGGLE_SIGNUP:
             return { ...state, token: action.payload, email: action.payloademail, username: action.payloadusername }
         case TOGGLE_TRY_LOCAL_SIGN:
-            return { token: action.payload, email: action.payloademail, username: action.payloadusername }
+            return {
+              token: action.payload,
+              email: action.payloademail,
+              username: action.payloadusername,
+              userlatitude : action.payloaduserlatitude,
+              userlongitude : action.payloaduserlongitude,
+            }
         case TOGGLE_SIGNOUT:
             return { token: null, email : null, username : null }
         default:
